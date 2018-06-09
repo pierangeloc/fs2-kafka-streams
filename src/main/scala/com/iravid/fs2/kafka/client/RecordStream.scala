15c47bd7384fa72c5e811b2f0807015e78ca0b54
Itamar Ravid <iravid@iravid.com>
2018-06-09 09:13:59 +0300

    WIP: output segments from recordstream

--- src/main/scala/com/iravid/fs2/kafka/client/RecordStream.scala
+++ src/main/scala/com/iravid/fs2/kafka/client/RecordStream.scala
@@ -1,23 +1,25 @@
 package com.iravid.fs2.kafka.client
 
 import cats.effect.Resource
-import cats.effect.concurrent.Deferred
+import cats.effect.concurrent.{ Deferred, Ref }
 import cats.effect.{ Concurrent, ConcurrentEffect, Sync, Timer }
 import cats.implicits._
 import com.iravid.fs2.kafka.EnvT
 import com.iravid.fs2.kafka.codecs.KafkaDecoder
 import com.iravid.fs2.kafka.model.{ ByteRecord, ConsumerMessage, Result }
-import fs2.{ async, Pipe, Stream }
+import fs2.{ async, Pipe, Segment, Stream }
 
 case class RecordStream[F[_], T](commitQueue: CommitQueue[F],
                                  records: Stream[F, ConsumerMessage[Result, T]])
 
 object RecordStream {
-  def resources[F[_]: ConcurrentEffect](settings: ConsumerSettings, consumer: Consumer[F])(
-    implicit timer: Timer[F]) =
+  def resources[F[_], T: KafkaDecoder](settings: ConsumerSettings, consumer: Consumer[F])(
+    implicit F: ConcurrentEffect[F],
+    timer: Timer[F]) =
     for {
       commitQueue         <- CommitQueue.create[F](settings.maxPendingCommits)
-      outQueue            <- async.boundedQueue[F, ByteRecord](settings.outputBufferSize)
+      outQueue            <- async.unboundedQueue[F, (Long, Segment[ByteRecord, Unit])]
+      currentBufferSize   <- Ref[F].of(0L)
       pollingLoopShutdown <- Deferred[F, Either[Throwable, Unit]]
       commits = commitQueue.queue.dequeue
       polls   = Stream(Poll) ++ Stream.repeatEval(timer.sleep(settings.pollInterval).as(Poll))
@@ -30,31 +32,45 @@ object RecordStream {
                   (consumer.commit(req.asOffsetMap).void.attempt >>= deferred.complete).void
                 case Right(Poll) =>
                   for {
-                    records <- consumer.poll(settings.pollTimeout, settings.wakeupTimeout)
-                    _       <- records.traverse_(outQueue.enqueue1)
+                    buf <- currentBufferSize.get
+                    _ <- if (buf >= settings.outputBufferSize) F.unit
+                        else
+                          for {
+                            records <- consumer.poll(settings.pollTimeout, settings.wakeupTimeout)
+                            recordCount = records.size.toLong
+                            _ <- currentBufferSize.update(_ + records.size)
+                            _ <- outQueue.enqueue1((recordCount, Segment.seq(records)))
+                          } yield ()
                   } yield ()
               }
               .compile
               .drain
           }
-    } yield (commitQueue, outQueue, pollingLoopShutdown.complete(Right(())))
+      outputStream = outQueue.dequeue
+        .evalMap {
+          case (size, segment) =>
+            currentBufferSize.update(_ - size).as(segment)
+        }
+        .flatMap(Stream.segment(_).covary[F])
+        .through(deserialize[F, T])
+    } yield (commitQueue, outputStream, pollingLoopShutdown.complete(Right(())))
 
   def apply[F[_]: ConcurrentEffect, T: KafkaDecoder](
     settings: ConsumerSettings,
     consumer: Consumer[F],
     subscription: Subscription)(implicit timer: Timer[F]): Resource[F, RecordStream[F, T]] =
-    Resource.make(resources[F](settings, consumer)) {
+    Resource.make(resources[F, T](settings, consumer)) {
       case (_, _, shutdown) =>
         for {
           _ <- consumer.unsubscribe
           _ <- shutdown
         } yield ()
     } flatMap {
-      case (commitQueue, outQueue, _) =>
+      case (commitQueue, outputStream, _) =>
         Resource.liftF {
           consumer
             .subscribe(subscription, _ => Sync[F].unit)
-            .as(RecordStream(commitQueue, outQueue.dequeue through deserialize[F, T]))
+            .as(RecordStream(commitQueue, outputStream))
         }
     }
 
